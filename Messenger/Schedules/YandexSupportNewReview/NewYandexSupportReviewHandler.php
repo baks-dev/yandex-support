71fd0cf2d0eae288cfaa07e051278cf59de274c7
kingstar <admin@baks.dev>
2025-01-16 23:09:41 +0300

    Baks Development

--- Messenger/Schedules/YandexSupportNewReview/NewYandexSupportReviewHandler.php
+++ Messenger/Schedules/YandexSupportNewReview/NewYandexSupportReviewHandler.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -42,6 +42,7 @@ use BaksDev\Yandex\Support\Api\Review\Get\GetListReviews\YandexGetListReviewsReq
 use BaksDev\Yandex\Support\Api\Review\Get\GetListReviews\YandexReviewDTO;
 use BaksDev\Yandex\Support\Types\ProfileType\TypeProfileYandexReviewSupport;
 use DateTimeImmutable;
+use DateTimeZone;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
