097ae6e2da2d5ea9e38b4cdd2a02d1573c7d7383
MichelAngelo <baksdevelopmen@gmail.com>
2025-08-21 22:58:15 +0300

    BaksDevelopment

--- Api/Messenger/Get/ListMessages/YandexListMessagesDTO.php
+++ Api/Messenger/Get/ListMessages/YandexListMessagesDTO.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -32,7 +32,7 @@ final class YandexListMessagesDTO
 {
 
     /** Идентификатор заказа. */
-    private int $order;
+    private ?int $order;
 
     /** Идентификатор сообщения. */
     private int $externalId;
@@ -85,7 +85,7 @@ final class YandexListMessagesDTO
         return !empty($result) ? $text.' '.implode(' ', $result) : trim($text);
     }
 
-    public function getOrder(): int
+    public function getOrder(): ?int
     {
         return $this->order;
     }
